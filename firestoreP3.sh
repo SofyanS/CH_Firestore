fd8d48262f0f1dcd9025fff8d270d09117688569
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-28 16:46:58 -0500

    fix directory

--- firestoreP3.sh
+++ firestoreP3.sh
@@ -4,7 +4,7 @@ export CLOUD_REGION=us-central1
 export GCLOUD_PROJECT=$(gcloud config list project --format "value(core.project)")
 
 # Create two devices
-cd ../sample-device
+cd sample-device
 gcloud iot devices create sample-device --region $CLOUD_REGION --registry $REGISTRY_ID --public-key path=./ec_public.pem,type=ES256
 gcloud iot devices create sample-binary --region $CLOUD_REGION --registry $REGISTRY_ID --public-key path=./ec_public.pem,type=ES256
 
